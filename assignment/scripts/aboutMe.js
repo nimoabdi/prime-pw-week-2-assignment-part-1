4c11ed41f5c46292c1e45c781163838e7e21a486
Nimo Abdi <nimoabdi318@gmail.com>
2022-04-10 16:01:29 -0500

    console logging the different variables 1-12

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,31 +1,32 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-
+let firstName = ' Nimo';
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
-
+let lastName = ' Abdi';
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
-
+let fullName = ' Nimo' + ' Abdi';
 // 4 - Console log the value of `fullName`
-
+console.log ('Nimo Abdi');
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
-
+let luckyNumber = 9;
 // 6 - Console log this sentence, adding in the variables you created above: 
 // 'My name is (full name), and I think (lucky number) is a winner!'.
 // Refer back to the videos if you need help with this one.
-
+sentence = ' My name is ' + fullName + ' and I think ' + luckyNumber + ' is a winner! ' 
+console.log (sentence);
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
-
+let adventurous = Boolean (false)
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
-
+let food = 'Tacos'
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
-
+let pets = 0;
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
-
+let friendsPets = 1;
 // 11 - Add two pets to your `pets` variable
-
+pets = 5;
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
-
+const allowedPets = 2;
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
 // if it's not true, console log "How about we stay home?"
 
